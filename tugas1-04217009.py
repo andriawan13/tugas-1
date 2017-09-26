0fce4b18013e62b0a59f574112d1e734071856f3
andriawan13 <32254520+andriawan13@users.noreply.github.com>
2017-09-26 10:25:30 -0700

    Add files via upload
    Tugas data diri

---
+++ tugas1-04217009.py
@@ -0,0 +1,9 @@
+nama = "Alfan Andriawan Subarli"
+nim = "04217009"
+umur = 19
+print ("namaku:" + nama)
+print ("nimku:" + nim)
+print ( "umur" )
+print (umur)
+print ("umur 5 tahun kedepan")
+print (umur +5)
